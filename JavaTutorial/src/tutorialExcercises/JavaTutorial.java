735659c77a344f6fa520414b223f76109be207ed
Thaveesha <Customer@DESKTOP-OHDG8R8>
2025-01-24 14:50:18 +0530

    Committed Variables

--- JavaTutorial/src/tutorialExcercises/JavaTutorial.java
+++ JavaTutorial/src/tutorialExcercises/JavaTutorial.java
@@ -1,9 +1,9 @@
-package defaultPackage;
+package tutorialExcercises;
 
 public class JavaTutorial {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world"); 
+		System.out.println("Hello world Later"); 
 
 	}
 
---
+++ JavaTutorial/src/tutorialExcercises/javaVariables.java
@@ -0,0 +1,23 @@
+package tutorialExcercises;
+
+import java.util.Scanner;
+
+public class javaVariables {
+	public static void main(String [] args) {
+		//Variable declarations
+		int a;
+		int b;
+		int Result;
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter Number 1"); //Prompt to Scan number 1 
+		a = scn.nextInt();//Scan number 1
+		System.out.println("Enter Number 2"); //Prompt to Scan number 2 
+		b = scn.nextInt(); //Scan number 2
+		
+		Result = a + b ; //Calculate Result
+		
+		System.out.println("The Result is " + Result); //Print Final Result 
+		scn.close();
+	}
+
+}
