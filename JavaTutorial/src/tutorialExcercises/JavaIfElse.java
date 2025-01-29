0d8fef0a78c9bf7e46685db73a4c5131cf54c90a
Thaveesha Gamage <gamage.thaveesha@gmail.com>
2025-01-29 15:22:06 +0530

    Commit JavaifElse

---
+++ JavaTutorial/src/tutorialExcercises/JavaIfElse.java
@@ -0,0 +1,32 @@
+package tutorialExcercises;
+
+import java.util.Scanner;
+
+public class JavaIfElse {
+	public static void main(String[] args) {
+		
+		int Num1;
+		int Num2;
+		//int com;
+		
+		//take Number 1
+		System.out.println("Enter Number 1");
+		Scanner sc = new Scanner(System.in);
+		Num1 = sc.nextInt();
+		System.out.println("Enter Number 2");
+		Num2 = sc.nextInt();
+		
+		if(Num1>Num2){
+			System .out.println("Largest Number is " + Num1);
+		}
+		else if(Num1 == Num2) {
+			System .out.println("Both numbers are equal");
+		}
+		else {
+			System .out.println("Largest Number is " + Num2);
+		}
+	
+	}
+	
+
+}
