b2f76dc27407b60eeadc9e179eb82b163a821d7d
Thaveesha Gamage <gamage.thaveesha@gmail.com>
2025-01-30 11:45:08 +0530

    Commit changes to JavaSwitchStatement Commit JavaSwitchStatementNew

--- JavaTutorial/src/tutorialExcercises/JavaSwitchStatement.java
+++ JavaTutorial/src/tutorialExcercises/JavaSwitchStatement.java
@@ -11,36 +11,27 @@ public class JavaSwitchStatement {
 		ex1 = sc.nextInt();
 		
 		switch (ex1) {
-			case 1:
-					System.out.println("Monday");
-					break;
+			case 1 -> System.out.println("Monday");
 					
-			case 2:
-				System.out.println("Tuesday");
-				break;
-				
-			case 3:
-				System.out.println("Wednesday");
-				break;
 					
-			case 4:
-				System.out.println("Thursday");
-				break;
+			case 2 -> System.out.println("Tuesday");
 				
-			case 5:
-				System.out.println("Friday");
-				break;
 				
-			case 6:
-				System.out.println("Saturday");
-				break;
+			case 3 -> System.out.println("Wednesday");
 				
-			case 7:
-				System.out.println("Sunday");
-				break;
+					
+			case 4 -> System.out.println("Thursday");
 				
-			default:
-				System.out.println("Incorrect number entered");
+				
+			case 5 -> System.out.println("Friday");
+				
+				
+			case 6 -> System.out.println("Saturday");
+				
+				
+			case 7 -> System.out.println("Sunday");
+				
+			default-> System.out.println("Incorrect number entered");
 				
 		}
 		sc.close();
